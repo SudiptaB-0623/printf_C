4ecea7ea5cbf6f21eca8bd6f3886ed3bd8e26188
SUDIPTA BASAK <84557979+SudiptaB-0623@users.noreply.github.com>
2021-09-02 15:05:44 +0530

    Update Evaluate the sine series.c

--- Looping Control Structures/Evaluate the sine series.c
+++ Looping Control Structures/Evaluate the sine series.c
@@ -7,16 +7,19 @@ int main()
     printf("\nMy name is Sudipta Basak \n");
 
 
-    int x,n,val=0;
+    int x,n,t=0;
+    float val=0;
 
     printf("\nEnter the value of 'x' : ");
     scanf("%d",&x);
     printf("\nEnter the value of 'n' : ");
     scanf("%d",&n);
 
+    printf("\nsin(%d) =  ",x);
+
     for(int i=1,j=1;i<=n;i++,j=j+2)
     {
-        int fact=1;
+        float fact=1;
         for(int k=j;k>=1;k--)
         {
             fact=fact*k;
@@ -25,18 +28,24 @@ int main()
         if(i%2==0)
         {
             val=val-(pow(x,j)/fact);
-
+            t++;
+            printf(" - (%d^%d)/%d!",x,j,j);
         }
         else
         {
             val=val+(pow(x,j)/fact);
-
-
+            t++;
+            if(t==1)
+            {
+                printf(" %d",x);
+                continue;
+            }
+            printf(" + (%d^%d)/%d!",x,j,j);
         }
 
     }
 
-    printf("\nsin(%d) = %d",x,val);
+    printf("\ = %.4f",val);
 
     return 0;
 }
