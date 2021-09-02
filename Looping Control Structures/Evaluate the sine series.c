1a198cccc4152d20aae7bd6333dd1fa2595fc811
SUDIPTA BASAK <84557979+SudiptaB-0623@users.noreply.github.com>
2021-09-02 08:52:43 +0530

    Rename L4-3.c to Evaluate the sine series.c

--- Looping Control Structures/Evaluate the sine series.c
+++ Looping Control Structures/Evaluate the sine series.c
@@ -1,4 +1,4 @@
-/*Evaluate the sine series, sin(x)= x- x^3/3! + x^5/5!–x^7/7!+ ……… to n terms*/
+/*Evaluate the sine series, sin(x)= x- x^3/3! + x^5/5!â€“x^7/7!+ â€¦â€¦â€¦ to n terms*/
 
 #include<stdio.h>
 
