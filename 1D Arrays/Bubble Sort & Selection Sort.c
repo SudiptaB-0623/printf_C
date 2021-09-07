b8c774249e46d130f3a6b2f8dae4d3f5dfd0ae03
SUDIPTA BASAK <84557979+SudiptaB-0623@users.noreply.github.com>
2021-09-07 10:38:47 +0530

    Update and rename Bubble Sort.c to Bubble Sort & Selection Sort.c

--- 1D Arrays/Bubble Sort & Selection Sort.c
+++ 1D Arrays/Bubble Sort & Selection Sort.c
@@ -13,7 +13,7 @@ int main()
     printf("\nEnter number of elements : ");
     scanf("%d",&num);
 
-    int ele[6];
+    int ele[num];
     printf("\nEnter the elements : ");
     for(int i=0;i<num;i++)
     {
@@ -22,7 +22,7 @@ int main()
 
     printf("\nSorting elements......  \nEnter 'a' for ascending order or 'd' for descending order : ");
     scanf("\n%c",&chr);
-    printf("\n** Order = %c",chr);
+
     switch(chr)
     {
         case 'a':   // bubble sorting procedure
@@ -36,37 +36,45 @@ int main()
                                 int temp = ele[j];
                                 ele[j] = ele[j+1];
                                 ele[j+1] = temp;
-                                printf("\nxyz");
+
                             }
                         }
                     }
                     printf("\nElements in ascending order : \n");
+                    for(int i=0;i<num;i++)
+                    {
+                        printf("%d   ",ele[i]);
+
+                    }
                     break;
 
-        case 'd':   // bubble sorting procedure
-                    for(int i=0; i<num-1; i++)         //pass
+        case 'd':   // selection sorting procedure
+                    for(int i = 0; i < num-1; i++)            // loop for number of pass
                     {
-                        for(int j=0; j<num-i-1; j++)
-                        {
-                            if(ele[j] < ele[j+1]) // comparison
-                            {
-                                // interchange
-                                int temp = ele[j];
-                                ele[j] = ele[j+1];
-                                ele[j+1] = temp;
-                                printf("\nabc");
-                            }
-                        }
+                         int pos = i, small = ele[i];
+                         for(int j=i+1; j<num; j++)          //loop for searching the smallest
+                         {
+                              if(small > ele[j])         // finding the smallest
+                              {
+                                   pos = j;           // pos for interchanging
+                                   small = ele[j];         // assigning current small value
+                              }
+                         }
+                         ele[pos] = ele[i];                 //interchanging values
+                         ele[i] = small;
                     }
                     printf("\nElements in descending order : \n");
+                    for(int i=num-1;i>=0;i--)
+                    {
+                        printf("%d   ",ele[i]);
+                    }
+                    break;
+        default:
+                    printf("\nInvalid input !!!");
                     break;
     }
 
-    for(int i=0;i<num;i++)
-    {
-        printf("%d   ",ele[i]);
-        //printf("\n123");
-    }
+
 
     return 0;
 }
